1cc901879ba9ac1417823901222f7aa9c6824cf3
Junji Shimagaki <junji.shimagaki@sony.com>
2018-11-14 14:05:20 +0900

    Adding data-forge package which fails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { DataFrame } from 'data-forge-beta'
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+
+  public ngOnInit(): void {
+    let df = new DataFrame({
+        columns: {
+            'regiment': ['Nighthawks', 'Nighthawks', 'Nighthawks', 'Nighthawks', 'Dragoons', 'Dragoons', 'Dragoons', 'Dragoons', 'Scouts', 'Scouts', 'Scouts', 'Scouts'],
+            'company': ['1st', '1st', '2nd', '2nd', '1st', '1st', '2nd', '2nd','1st', '1st', '2nd', '2nd'],
+            'TestScore': [4, 24, 31, 2, 3, 4, 24, 31, 2, 3, 2, 3]
+        }
+    });
+    const pivotted = df.pivot(["regiment", "company"], "TestScore", testScores => testScores.average());
+    console.log(pivotted.toArray())
+  }
+
 }
